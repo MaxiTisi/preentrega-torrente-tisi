8234f3e5dd607365eafb620197212f4934f850b4
maxi tisi <tusitioweb22@gmail.com>
2023-03-02 17:04:12 -0300

    preentrega

---
+++ java.js
@@ -0,0 +1,22 @@
+
+
+/*/let nombre = prompt ("pon tu nombre aqui");
+alert ("hola " + nombre)/*/
+
+let mesUno =parseFloat (prompt("igreso bruto del mes 1"))
+
+let mesDos =parseFloat (prompt("igreso bruto del mes 2"))
+
+let mesTres =parseFloat (prompt("igreso bruto del mes 3"))
+
+
+let resultadoTrimestral = (mesUno + mesDos + mesTres)
+
+alert ( "tu ganancia bruta trimestral es $" + resultadoTrimestral)
+
+
+
+
+
+
+
